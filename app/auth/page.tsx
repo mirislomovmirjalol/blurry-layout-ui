0c3ee070c029e95e9e477b0a997926ade760c9b9
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2024-03-27 22:23:54 +0000

    feat: minimal auth page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,7 +1,20 @@
+import {Button} from "@/components/ui/button";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+
 export default function AuthPage() {
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className="flex justify-center mt-64">
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>Currently sign in supports only with google.</CardDescription>
+                </CardHeader>
+                <CardContent className="grid gap-4">
+                    <Button className="w-full" variant="default">
+                        Sign in with Google
+                    </Button>
+                </CardContent>
+            </Card>
         </div>
     );
 }

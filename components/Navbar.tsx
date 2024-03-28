0f6d30ae36304c619fc2143fab5803960ee230b5
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2024-03-28 01:56:09 +0000

    fix: unique key

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,11 +11,10 @@ const Navbar = () => {
         <nav className="md:top-4 md:bottom-full fixed bottom-4 w-full mx-auto z-10">
             <ul className="flex flex-row space-x-1 justify-between items-center rounded-full p-1 text-muted-foreground border backdrop-blur-md bg-background/50 max-w-[350px] mx-auto">
                 {navlinks.map((link) => (
-
-                    <TooltipProvider delayDuration={0}>
+                    <TooltipProvider key={link.href} delayDuration={0}>
                         <Tooltip>
                             <TooltipTrigger asChild>
-                                <Link key={link.href} href={link.href}
+                                <Link href={link.href}
                                       className={cn("py-4 w-full flex justify-center hover:bg-accent rounded-full transition-all duration-300", pathname === link.href ? "bg-accent text-accent-foreground" : "")}>
                                     <li>
                                         {link.icon}
